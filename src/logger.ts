041473cfc5f5f5fdd3767c81d6e03ad40d60b835
Max Michalec <mmich8749@gmail.com>
2023-12-06 10:01:28 -0500

    removed need for env variables for logger

--- src/logger.ts
+++ src/logger.ts
@@ -7,22 +7,25 @@ export const log = new Logger({
 	minLevel: 7,
 }); // no messages by default
 
-if (process.env.LOG_LEVEL === "1") {
-	// only log.info, log.warn, log.error, and log.fatal
-	log.settings.minLevel = 3;
-} else if (process.env.LOG_LEVEL === "2") {
-	// all logs
-	log.settings.minLevel = 0;
-}
+// if (process.env.LOG_LEVEL === "1") {
+// 	// only log.info, log.warn, log.error, and log.fatal
+// 	log.settings.minLevel = 3;
+// } else if (process.env.LOG_LEVEL === "2") {
+// 	// all logs
+// 	log.settings.minLevel = 0;
+// }
+
+log.settings.minLevel = 0;
 
 // Set output file
 log.attachTransport((logObj) => {
 	// Must have a LOG_FILE environment variable set
-	if (process.env.LOG_FILE === undefined) {
-		console.error("LOG_FILE environment variable not set");
-		process.exit(1);
-	}
-	appendFileSync(process.env.LOG_FILE, JSON.stringify(logObj) + "\n");
+	// if (process.env.LOG_FILE === undefined) {
+	// 	console.error("LOG_FILE environment variable not set");
+	// 	process.exit(1);
+	// }
+	// appendFileSync(process.env.LOG_FILE, JSON.stringify(logObj) + "\n");
+	appendFileSync("log.txt", JSON.stringify(logObj) + "\n");
 	// Log to file in format: [2021-03-31T21:00:00.000Z] [INFO] Logger: Hello World
 	// appendFileSync(
 	// 	process.env.LOG_FILE,
