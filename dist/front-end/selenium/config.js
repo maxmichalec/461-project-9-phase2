13f3a8be3643cb5ce202b4cf94953e79675ee6fe
Caroline Gilbert <79814305+carolinecgilbert@users.noreply.github.com>
2023-12-11 17:53:02 -0500

    Delete dist directory

--- dist/front-end/selenium/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fs = exports.logFilePath = exports.baseUrl = void 0;
-exports.baseUrl = 'http://localhost:4200/home';
-exports.logFilePath = 'selenium-test.log';
-exports.fs = require('fs');
--- dist/front-end/src/app/api/services.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ApiService = void 0;
-var api_service_1 = require("./services/api.service");
-Object.defineProperty(exports, "ApiService", { enumerable: true, get: function () { return api_service_1.ApiService; } });
