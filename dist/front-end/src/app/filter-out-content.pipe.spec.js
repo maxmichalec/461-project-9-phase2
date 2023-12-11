b2f96bc3b6e9c49aaa3401f2a5413d54f8b4fd87
Caroline Gilbert <79814305+carolinecgilbert@users.noreply.github.com>
2023-12-11 14:47:32 -0500

    Delete dist directory

--- dist/front-end/src/app/filter-out-content.pipe.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const filter_out_content_pipe_1 = require("./filter-out-content.pipe");
-describe('FilterOutContentPipe', () => {
-    it('create an instance', () => {
-        const pipe = new filter_out_content_pipe_1.FilterOutContentFieldPipe();
-        expect(pipe).toBeTruthy();
-    });
-});
