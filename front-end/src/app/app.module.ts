20434bfc7c7cbb2667b3102af565bb14da413c12
Caroline Gilbert <carolinecgilbert6@gmail.com>
2023-12-11 17:37:51 -0500

    ng test passes

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,43 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApiService } from './api/services';
-import { PackagesComponent } from './packages/packages.component';
-import { ResetComponent } from './reset/reset.component';
-import { PackageComponent } from './package/package.component';
-import { HomeComponent } from './home/home.component';
-import { PackagebyNameComponent } from './packageby-name/packageby-name.component';
-import { PackagebyRegexComponent } from './packageby-regex/packageby-regex.component';
-import { FilterOutContentFieldPipe } from './filter-out-content.pipe';
-import { CommonModule } from '@angular/common';
 
+import { HomeModule } from './home/home.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PackagesComponent,
-    ResetComponent,
-    PackageComponent,
-    HomeComponent,
-    PackagebyNameComponent,
-    PackagebyRegexComponent,
-    FilterOutContentFieldPipe ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    CommonModule,
-  ],
-  providers:  [
-    ApiService,
-  ],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [BrowserModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule, HomeModule],
+	providers: [ApiService],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
